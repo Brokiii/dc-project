dfce1af4562ed2892f323db4427c963f1e6c0a86
Jakub <kisiel.jakub00@gmail.com>
2022-10-30 20:22:10 +0100

    changed db schema

--- backend/sql/init.sql
+++ backend/sql/init.sql
@@ -1,52 +1,55 @@
 CREATE TABLE "user"(
-                       "user_id" SERIAL NOT NULL,
+                       "id" INTEGER NOT NULL,
                        "login" VARCHAR(50) NOT NULL,
-                       "hashed_password" VARCHAR(200) NULL,
-                       "account_type" VARCHAR(50) NOT NULL,
+                       "hashedPassword" VARCHAR(200) NOT NULL,
+                       "accountType" VARCHAR(50) NOT NULL,
                        "email" VARCHAR(50) NOT NULL,
                        "name" VARCHAR(50) NOT NULL,
                        "surname" VARCHAR(50) NOT NULL
 );
 ALTER TABLE
-    "user" ADD PRIMARY KEY("user_id");
+    "user" ADD PRIMARY KEY("id");
 CREATE TABLE "insurance"(
-                            "insurance_id" SERIAL NOT NULL,
-                            "good_type" VARCHAR(50) NOT NULL,
-                            "client_id" INTEGER NOT NULL,
-                            "insurance_type" VARCHAR(50) NOT NULL
+                            "id" INTEGER NOT NULL,
+                            "goodType" VARCHAR(50) NOT NULL,
+                            "clientId" INTEGER NOT NULL,
+                            "insuranceType" VARCHAR(50) NOT NULL,
+                            "agentId" INTEGER NOT NULL
 );
 ALTER TABLE
-    "insurance" ADD PRIMARY KEY("insurance_id");
+    "insurance" ADD PRIMARY KEY("id");
 CREATE TABLE "attachment"(
                              "id" INTEGER NOT NULL,
                              "content" bytea NOT NULL,
-                             "insurance_id" INTEGER NOT NULL,
+                             "insuranceId" INTEGER NOT NULL,
                              "uuid" UUID NOT NULL
 );
 ALTER TABLE
     "attachment" ADD PRIMARY KEY("id");
-CREATE TABLE "loss_report"(
-                              "loss_id" SERIAL NOT NULL,
-                              "insurance_id" INTEGER NOT NULL,
-                              "stage" VARCHAR(500) NOT NULL,
-                              "decision_comment" VARCHAR(500) NOT NULL,
-                              "considered_favourably" BOOLEAN NOT NULL
+CREATE TABLE "loss"(
+                       "id" INTEGER NOT NULL,
+                       "insuranceId" INTEGER NOT NULL,
+                       "stage" VARCHAR(500) NOT NULL,
+                       "decisionComment" VARCHAR(500) NOT NULL,
+                       "consideredFavourably" BOOLEAN NOT NULL
 );
 ALTER TABLE
-    "loss_report" ADD PRIMARY KEY("loss_id");
+    "loss" ADD PRIMARY KEY("id");
 CREATE TABLE "appellation"(
-                              "appellation_id" SERIAL NOT NULL,
-                              "loss_report_id" INTEGER NOT NULL,
-                              "decision_comment" VARCHAR(500) NOT NULL,
-                              "client_comment" VARCHAR(500) NOT NULL
+                              "id" INTEGER NOT NULL,
+                              "lossReportId" INTEGER NOT NULL,
+                              "decisionComment" VARCHAR(500) NOT NULL,
+                              "clientComment" VARCHAR(500) NOT NULL
 );
 ALTER TABLE
-    "appellation" ADD PRIMARY KEY("appellation_id");
+    "appellation" ADD PRIMARY KEY("id");
 ALTER TABLE
-    "insurance" ADD CONSTRAINT "insurance_clientid_foreign" FOREIGN KEY("client_id") REFERENCES "user"("user_id");
+    "insurance" ADD CONSTRAINT "insurance_clientid_foreign" FOREIGN KEY("clientId") REFERENCES "user"("id");
 ALTER TABLE
-    "attachment" ADD CONSTRAINT "attachment_insuranceid_foreign" FOREIGN KEY("insurance_id") REFERENCES "insurance"("insurance_id");
+    "insurance" ADD CONSTRAINT "insurance_agentid_foreign" FOREIGN KEY("agentId") REFERENCES "user"("id");
 ALTER TABLE
-    "loss_report" ADD CONSTRAINT "loss_report_insurance_id_foreign" FOREIGN KEY("insurance_id") REFERENCES "insurance"("insurance_id");
+    "attachment" ADD CONSTRAINT "attachment_insuranceid_foreign" FOREIGN KEY("insuranceId") REFERENCES "insurance"("id");
 ALTER TABLE
-    "appellation" ADD CONSTRAINT "appellation_loss_report_id_foreign" FOREIGN KEY("loss_report_id") REFERENCES "loss_report"("loss_id");
+    "loss" ADD CONSTRAINT "loss_insuranceid_foreign" FOREIGN KEY("insuranceId") REFERENCES "insurance"("id");
+ALTER TABLE
+    "appellation" ADD CONSTRAINT "appellation_lossreportid_foreign" FOREIGN KEY("lossReportId") REFERENCES "loss"("id");
