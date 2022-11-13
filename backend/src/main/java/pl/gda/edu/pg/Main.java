59ae0f21225fccecdd0746b1a3e609b99cec5559
s180163 <hubertbrokos@wp.pl>
2022-11-13 22:31:04 +0100

    fixed cors

--- backend/src/main/java/pl/gda/edu/pg/Main.java
+++ backend/src/main/java/pl/gda/edu/pg/Main.java
@@ -2,6 +2,16 @@ package pl.gda.edu.pg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class Main {
