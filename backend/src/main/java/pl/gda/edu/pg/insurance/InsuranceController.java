fa060ef45df0180474c8d3c295187cbb8d0a9aaf
s180163 <hubertbrokos@wp.pl>
2022-11-26 16:48:50 +0100

    added mail service

--- backend/src/main/java/pl/gda/edu/pg/insurance/InsuranceController.java
+++ backend/src/main/java/pl/gda/edu/pg/insurance/InsuranceController.java
@@ -24,6 +24,7 @@ import pl.gda.edu.pg.insurance.entity.entity.InsuranceUpdateAgentRequest;
 import pl.gda.edu.pg.loss.LossService;
 import pl.gda.edu.pg.loss.entity.Loss;
 
+import javax.mail.MessagingException;
 import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -85,7 +86,7 @@ public class InsuranceController {
     @GetMapping("/pdf")
     public ResponseEntity<InputStreamResource> getPdfFromInsurance(
             @RequestParam int id
-    ) throws DocumentException, FileNotFoundException {
+    ) throws DocumentException, FileNotFoundException, MessagingException {
         ByteArrayInputStream bis = insuranceService.createPdfFromInsurance(id);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Disposition", "inline; filename=insurance.pdf");
